dcfed99b0196dfb6c82a61b42495ecdcf8106d7a
tusharhow <tusharsoftdev@gmail.com>
2022-12-03 23:15:43 -0800

    indicator add

--- lib/controllers/auth_controllers.dart
+++ lib/controllers/auth_controllers.dart
@@ -1,23 +1,31 @@
 import 'dart:convert';
+import 'package:get/get.dart';
+
 import '../models/login_response.dart';
 import 'package:http/http.dart' as http;
 import '../models/register_response.dart';
 
-
-class AuthControllers {
+class AuthControllers extends GetxController {
   static const login = "https://reqres.in/api/login";
   static const register = "https://reqres.in/api/register";
 
+  bool isLoading = false;
+
   // login
   Future<LoginResponse> loginRequest() async {
+    isLoading = true;
+    update();
     var response = await http.post(Uri.parse(login), body: {
       "email": 'eve.holt@reqres.in',
-      "password": 'cityslicka',
+      "password": 'pistol',
     });
+    isLoading = false;
+    update();
     if (response.statusCode == 200) {
-      return LoginResponse.fromJson(json.decode(response.body));
+      print(response.body);
+      return LoginResponse.fromJson(jsonDecode(response.body));
     } else {
-      throw Exception('Failed to login!');
+      throw Exception('Login Failed');
     }
   }
 
--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,37 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_api/controllers/auth_controllers.dart';
+import 'package:get/get.dart';
 
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final AuthControllers authControllers = AuthControllers();
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                authControllers.loginRequest();
-              },
-              child: const Text('Login With API'),
-            ),
-          ),
-          const SizedBox(
-            height: 20,
-          ),
-          Center(
-            child: ElevatedButton(
-              onPressed: () {
-                authControllers.registerRequest();
-              },
-              child: const Text('Register With API'),
-            ),
-          ),
-        ],
-      ),
+      body: GetBuilder<AuthControllers>(
+          init: AuthControllers(),
+          builder: (authControllers) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                authControllers.isLoading
+                    ? const CircularProgressIndicator()
+                    : Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            authControllers.loginRequest();
+                          },
+                          child: const Text('Login With API'),
+                        ),
+                      ),
+                const SizedBox(
+                  height: 20,
+                ),
+                authControllers.isLoading
+                    ? const CircularProgressIndicator()
+                    : Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            authControllers.registerRequest();
+                          },
+                          child: const Text('Register With API'),
+                        ),
+                      ),
+              ],
+            );
+          }),
     );
   }
 }
